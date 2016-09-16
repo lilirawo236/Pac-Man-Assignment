dc2f48b1ed7c576a0e40bb7cc6c44f6f6b468b9f
Lily <Lily@BRO>
2016-09-16 13:01:01 -0400

    Add project files.

---
+++ Logo/Form1.Designer.cs
@@ -0,0 +1,77 @@
+﻿namespace Logo
+{
+    partial class window1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.startButton = new System.Windows.Forms.Button();
+            this.countdownBox = new System.Windows.Forms.Label();
+            this.SuspendLayout();
+            // 
+            // startButton
+            // 
+            this.startButton.BackColor = System.Drawing.SystemColors.InfoText;
+            this.startButton.Font = new System.Drawing.Font("Rockwell", 20.25F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.startButton.ForeColor = System.Drawing.SystemColors.Control;
+            this.startButton.Location = new System.Drawing.Point(102, 110);
+            this.startButton.Name = "startButton";
+            this.startButton.Size = new System.Drawing.Size(177, 95);
+            this.startButton.TabIndex = 0;
+            this.startButton.Text = "Play Game";
+            this.startButton.UseVisualStyleBackColor = false;
+            this.startButton.Click += new System.EventHandler(this.startButton_Click);
+            // 
+            // countdownBox
+            // 
+            this.countdownBox.Font = new System.Drawing.Font("Rockwell", 12F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.countdownBox.ForeColor = System.Drawing.SystemColors.ControlText;
+            this.countdownBox.Location = new System.Drawing.Point(146, 281);
+            this.countdownBox.Name = "countdownBox";
+            this.countdownBox.Size = new System.Drawing.Size(224, 28);
+            this.countdownBox.TabIndex = 1;
+            // 
+            // window1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.BackColor = System.Drawing.SystemColors.InfoText;
+            this.ClientSize = new System.Drawing.Size(382, 318);
+            this.Controls.Add(this.countdownBox);
+            this.Controls.Add(this.startButton);
+            this.Name = "window1";
+            this.Text = "Christmas!";
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Button startButton;
+        private System.Windows.Forms.Label countdownBox;
+    }
+}
+
---
+++ Logo/Form1.cs
@@ -0,0 +1,60 @@
+﻿/*This program was created by Lily Raworth Marshall in September, 2016
+for the purpose of a school project in ICS3U*/
+using System;
+using System.Collections.Generic;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using System.Threading;
+
+namespace Logo
+{
+    public partial class window1 : Form
+    {
+        public window1()
+        {
+            InitializeComponent();
+        }
+
+        private void startButton_Click(object sender, EventArgs e)
+        {
+            startButton.Visible = false; //makes start button disappear
+            this.BackColor = Color.White; //changes it to loading screen
+
+            /*Refresh(); //new screen and the countdown starts at 3
+            countdownBox.Text = "The game will start in: 3";
+            Thread.Sleep(1000);
+
+            Refresh(); //countdown is at 2
+            countdownBox.Text = "The game will start in: 2";
+            Thread.Sleep(1000);
+
+            Refresh(); //countdown is at 1
+            countdownBox.Text = "The game will start in: 1";
+            Thread.Sleep(1000);
+
+            Refresh();
+            countdownBox.Text = "The game will start in: 0"; //it won't actually
+            Thread.Sleep(1000);*/ //show the count of zero
+            this.BackColor = Color.DarkBlue;
+
+            Refresh(); //the countdown text disappears
+            countdownBox.Visible = false;
+
+            Graphics formGraphics = this.CreateGraphics();
+            Pen drawPen = new Pen(Color.Black, 5);
+            SolidBrush drawBrush = new SolidBrush (Color.Yellow);
+            SolidBrush drawBrush2 = new SolidBrush(Color.DarkBlue);
+
+            formGraphics.DrawEllipse(drawPen, 87, 60, 200, 200);
+            formGraphics.FillEllipse(drawBrush, 87, 60, 200, 200);
+            formGraphics.FillEllipse(drawBrush2, 91, 80, 150, 150);
+            formGraphics.DrawEllipse(drawPen, 91, 80, 150, 150);
+            Thread.Sleep(1000);
+
+        }
+    }
+}
